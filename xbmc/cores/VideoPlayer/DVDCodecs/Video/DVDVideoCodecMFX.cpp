3fa04b14c4cf9800c8445c7eaaef00e5187d4562
Anton Fedchin <afedchin@ruswizards.com>
2016-03-06 16:31:03 +0300

    fixup! fixed: EOS nalu causes out of sync for views.

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMFX.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMFX.cpp
@@ -403,6 +403,17 @@ int CDVDVideoCodecMFX::Decode(uint8_t* buffer, int buflen, double dts, double pt
       std::copy(buffer, buffer + buflen, std::back_inserter(m_buff));
     }
 
+    CH264Nalu nalu;
+    nalu.SetBuffer(m_buff.data(), m_buff.size(), 0);
+    while (nalu.ReadNext()) 
+    {
+      if (nalu.GetType() == NALU_TYPE_EOSEQ) 
+      {
+        // This is rather ugly, and relies on the bitstream being AnnexB, so simply overwriting the EOS NAL with zero works.
+        // In the future a more elaborate bitstream filter might be advised
+        memset(m_buff.data() + nalu.GetNALPos(), 0, 4);
+      }
+    }
     bs.Data = m_buff.data();
     bs.DataLength = m_buff.size();
     bs.MaxLength = bs.DataLength;
@@ -590,12 +601,13 @@ bool CDVDVideoCodecMFX::GetPicture(DVDVideoPicture* pDvdVideoPicture)
     pFrame->color_range = 0;
     pFrame->iFlags = DVP_FLAG_ALLOCATED;
     pFrame->dts = DVD_NOPTS_VALUE;
-    if (!(pBaseView->surface.Data.DataFlag & MFX_FRAMEDATA_ORIGINAL_TIMESTAMP))
+    pFrame->pts = DVD_NOPTS_VALUE;
+    /*if (!(pBaseView->surface.Data.DataFlag & MFX_FRAMEDATA_ORIGINAL_TIMESTAMP))
       pBaseView->surface.Data.TimeStamp = MFX_TIMESTAMP_UNKNOWN;
     if (pBaseView->surface.Data.TimeStamp != MFX_TIMESTAMP_UNKNOWN) 
       pFrame->pts = static_cast<double>(pBaseView->surface.Data.TimeStamp);
     else 
-      pFrame->pts = DVD_NOPTS_VALUE;
+      pFrame->pts = DVD_NOPTS_VALUE;*/
     pFrame->mvc = m_pRenderPicture;
 
     return true;
@@ -723,4 +735,95 @@ void CDVDVideoCodecMFX::SetStereoMode(CDVDStreamInfo &hints)
   if (hints.stereo_mode.empty() || hints.stereo_mode.compare("mono") == 0)
     hints.stereo_mode = "mvc_lr";
   m_stereoMode = hints.stereo_mode;
+}
+
+void CH264Nalu::SetBuffer(const uint8_t* pBuffer, size_t nSize, int nNALSize)
+{
+  m_pBuffer = pBuffer;
+  m_nSize = nSize;
+  m_nNALSize = nNALSize;
+  m_nCurPos = 0;
+  m_nNextRTP = 0;
+
+  m_nNALStartPos = 0;
+  m_nNALDataPos = 0;
+
+  // In AnnexB, the buffer is not guaranteed to start on a NAL boundary
+  if (nNALSize == 0 && nSize > 0)
+    MoveToNextAnnexBStartcode();
+}
+
+bool CH264Nalu::MoveToNextAnnexBStartcode()
+{
+  if (m_nSize < 4)
+    goto notfound;
+
+  size_t nBuffEnd = m_nSize - 4;
+
+  for (size_t i = m_nCurPos; i <= nBuffEnd; i++) 
+  {
+    if ((*((DWORD*)(m_pBuffer + i)) & 0x00FFFFFF) == 0x00010000) 
+    {
+      // Found next AnnexB NAL
+      m_nCurPos = i;
+      return true;
+    }
+  }
+
+notfound:
+  m_nCurPos = m_nSize;
+  return false;
+}
+
+bool CH264Nalu::MoveToNextRTPStartcode()
+{
+  if (m_nNextRTP < m_nSize) 
+  {
+    m_nCurPos = m_nNextRTP;
+    return true;
+  }
+
+  m_nCurPos = m_nSize;
+  return false;
+}
+
+bool CH264Nalu::ReadNext()
+{
+  if (m_nCurPos >= m_nSize) 
+    return false;
+
+  if ((m_nNALSize != 0) && (m_nCurPos == m_nNextRTP)) 
+  {
+    if (m_nCurPos + m_nNALSize >= m_nSize) 
+      return false;
+    // RTP Nalu type : (XX XX) XX XX NAL..., with XX XX XX XX or XX XX equal to NAL size
+    m_nNALStartPos = m_nCurPos;
+    m_nNALDataPos = m_nCurPos + m_nNALSize;
+
+    // Read Length code from the buffer
+    unsigned nTemp = 0;
+    for (int i = 0; i < m_nNALSize; i++)
+      nTemp = (nTemp << 8) + m_pBuffer[m_nCurPos++];
+
+    m_nNextRTP += nTemp + m_nNALSize;
+    MoveToNextRTPStartcode();
+  }
+  else 
+  {
+    // Remove trailing bits
+    while (m_pBuffer[m_nCurPos] == 0x00 && ((*((DWORD*)(m_pBuffer + m_nCurPos)) & 0x00FFFFFF) != 0x00010000))
+      m_nCurPos++;
+
+    // AnnexB Nalu : 00 00 01 NAL...
+    m_nNALStartPos = m_nCurPos;
+    m_nCurPos += 3;
+    m_nNALDataPos = m_nCurPos;
+    MoveToNextAnnexBStartcode();
+  }
+
+  forbidden_bit = (m_pBuffer[m_nNALDataPos] >> 7) & 1;
+  nal_reference_idc = (m_pBuffer[m_nNALDataPos] >> 5) & 3;
+  nal_unit_type = (NALU_TYPE)(m_pBuffer[m_nNALDataPos] & 0x1f);
+
+  return true;
 }
--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMFX.h
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMFX.h
@@ -68,6 +68,65 @@ public:
   void (*free_buffers)(CMVCPicture* pic);
 };
 
+typedef enum
+{
+  NALU_TYPE_UNKNOWN = 0,
+  NALU_TYPE_SLICE = 1,
+  NALU_TYPE_DPA = 2,
+  NALU_TYPE_DPB = 3,
+  NALU_TYPE_DPC = 4,
+  NALU_TYPE_IDR = 5,
+  NALU_TYPE_SEI = 6,
+  NALU_TYPE_SPS = 7,
+  NALU_TYPE_PPS = 8,
+  NALU_TYPE_AUD = 9,
+  NALU_TYPE_EOSEQ = 10,
+  NALU_TYPE_EOSTREAM = 11,
+  NALU_TYPE_FILL = 12
+} NALU_TYPE;
+
+
+class CH264Nalu
+{
+protected:
+  int        forbidden_bit = 0;                 //! should be always FALSE
+  int        nal_reference_idc = 0;                 //! NALU_PRIORITY_xxxx
+  NALU_TYPE  nal_unit_type = NALU_TYPE_UNKNOWN; //! NALU_TYPE_xxxx
+
+  size_t     m_nNALStartPos = 0;                 //! NALU start (including startcode / size)
+  size_t     m_nNALDataPos = 0;                 //! Useful part
+
+  const BYTE *m_pBuffer = nullptr;
+  size_t     m_nCurPos = 0;
+  size_t     m_nNextRTP = 0;
+  size_t     m_nSize = 0;
+  int        m_nNALSize = 0;
+
+  bool      MoveToNextAnnexBStartcode();
+  bool      MoveToNextRTPStartcode();
+
+public:
+  CH264Nalu() { SetBuffer(nullptr, 0, 0); }
+  NALU_TYPE GetType() const { return nal_unit_type; }
+  bool      IsRefFrame() const { return (nal_reference_idc != 0); }
+
+  size_t    GetDataLength() const { return m_nCurPos - m_nNALDataPos; }
+  const uint8_t *GetDataBuffer() { return m_pBuffer + m_nNALDataPos; }
+  size_t    GetRoundedDataLength() const
+  {
+    size_t nSize = m_nCurPos - m_nNALDataPos;
+    return nSize + 128 - (nSize % 128);
+  }
+
+  size_t    GetLength() const { return m_nCurPos - m_nNALStartPos; }
+  const uint8_t *GetNALBuffer() { return m_pBuffer + m_nNALStartPos; }
+  size_t    GetNALPos() { return m_nNALStartPos; }
+  bool      IsEOF() const { return m_nCurPos >= m_nSize; }
+
+  void      SetBuffer(const uint8_t *pBuffer, size_t nSize, int nNALSize);
+  bool      ReadNext();
+};
+
 class CDVDVideoCodecMFX : public CDVDVideoCodec
 {
 public:
