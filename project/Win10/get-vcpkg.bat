9ac02ba54727ec665ffeed15eca1cbb6b3a14192
Anton Fedchin <anightik@gmail.com>
2017-06-16 11:59:53 +0300

    [vcpkg] allow using pre-built packages

--- project/Win10/get-vcpkg.bat
+++ project/Win10/get-vcpkg.bat
@@ -1,8 +1,13 @@
 @echo off
+set repo=https://github.com/afedchin/vcpkg
+set version=v0.0.1
+set zip=%cd%\..\Win32BuildSetup\tools\7z\7za.exe
+set wget=%cd%\..\BuildDependencies\bin\wget.exe
+set download=yes
 
 if not exist vcpkg (
   echo Cloning vcpkg...
-  call git clone -b kodi-deps --single-branch https://github.com/afedchin/vcpkg.git
+  call git clone -b kodi-deps --single-branch %repo%.git
 )
 
 set arch=x86
@@ -13,14 +18,46 @@ if "%1" == "arm" (
   set arch=arm
 )
 
-echo Building and installing vcpkg...
 pushd vcpkg
   if not exist vcpkg.exe (
-    echo A | ./bootstrap-vcpkg.bat
+    echo Building and installing vcpkg...
+    echo A | .\bootstrap-vcpkg.bat
   )
+  echo Installing common tools
+  call vcpkg install libfribidi swig
+popd
+
+if not "%download%" == "yes" goto install
+set archive=vcpkg-export-%arch%-uwp-%version%.7z
+set URL=%repo%/releases/download/%version%/%archive%
+
+if exist %archive% goto extract
+echo Downloading pre-built kodi-deps:%arch%-uwp
+%wget% --tries=5 --retry-connrefused --waitretry=2 --no-check-certificate -c %URL%
+
+:extract
+if not exist %archive% goto install
+
+echo Extracting pre-built kodi-deps:%arch%-uwp
+%zip% x %archive% -o.\temp -y
+for /D %%G in ("%cd%\temp\*") do (
+  xcopy .\temp\%%~nxG %cd%\vcpkg /iycqs
+  goto forend
+)
+:forend
+
+if exist temp (
+  rd /S /Q .\temp
+)
+
+if exist vcpkg\installed\%arch%-uwp\share\kodi-deps goto end
+
+:install
+pushd vcpkg
   echo Building and installing kodi-deps:%arch%-uwp
   rem need to install windows version first to create include files with win32 tools
-  call vcpkg install libfribidi
   call vcpkg install kodi-deps:%arch%-uwp
 popd
+
+:end
 echo kodi-deps:%arch%-uwp installed
